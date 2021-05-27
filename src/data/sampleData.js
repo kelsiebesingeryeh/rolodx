6ff544c2959a6473e23bdc5a582ca573453beda4
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-05-27 11:27:38 -0600

    Add sample data for testing

---
+++ src/data/sampleData.js
@@ -0,0 +1,15 @@
+const sampleData = [
+    {
+        "first name": "Kelsie",
+        "last name": "Besinger",
+        "email": "kelsiebesinger@gmail.com",
+        "phone": "646-246-7341",
+        "birthday": "June, 5, 1990",
+        "date of last outreach": "5/27/2021",
+        "company": "turing",
+        "position": "frontend engineering student",
+        "notes": "TBD",
+        "social": "tbd",
+        "actions": "tbd"
+    }
+]
