ab70d6ce500e9da03f0c8f010764f2d62674b11c
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-06-02 10:20:59 -0600

    Add data to rows

--- src/components/Contact/ContactList/ContactList.js
+++ src/components/Contact/ContactList/ContactList.js
@@ -9,14 +9,15 @@ const ContactList = ({ name, email, phone, birthday, outreachdate, company, posi
       { field: "email", headerName: "Email", width: 120 },
       { field: "phone", headerName: "Phone", width: 150 },
       { field: "birthday", headerName: "Birthday", width: 150 },
-      { field: "date of last outreach", headerName: "Date of last outreach", width: 150 },
+      { field: "outreachdate", headerName: "Date of last outreach", width: 150 },
       { field: "company", headerName: "Company", width: 150 },
       { field: "position", headerName: "Position", width: 150 },
       { field: "notes", headerName: "Notes", width: 150 },
     ];
+    console.log(name)
 
     const rows = [
-
+        {id: name, email: email, phone: phone, birthday: birthday, outreachdate: outreachdate, company: company, position: position, notes: notes}
     ]
 
     return (
